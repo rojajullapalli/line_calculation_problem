fc1a4c2578fc0e26181605c108bd2c7d2ab50b09
rojajullapalli <rojajules@gmail.com>
2021-08-10 13:18:10 +0530

    welcome to line comparision program

---
+++ src/com/roja/length/Linecomparision.java
@@ -0,0 +1,14 @@
+package com.roja.length;
+
+/**
+ * Linecomparision program is for welcome to line comparision program
+ *
+ * @author :roja
+ * @version :1.0
+ * @since :10-08-2021
+ */
+public class Linecomparision {
+    public static void main(String[] args) {
+        System.out.println("welcome to line comparision program");
+    }
+}
